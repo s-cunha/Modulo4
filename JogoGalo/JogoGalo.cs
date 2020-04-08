3329fe6996b384f3c6f5c7f96fe0e41a4a42fd52
s-cunha <s.cunha@recodme.pt>
2020-04-08 17:54:59 +0100

    jogo do galo2

--- JogoGalo/JogoGalo.cs
+++ JogoGalo/JogoGalo.cs
@@ -57,52 +57,98 @@ namespace JogoGalo
             for (count = 1; count < 10; count++)
             {
                 var n = Console.ReadLine();
+               
                 if (count % 2 != 0) {
                     switch (n)
                     { case "1":
                             n1 = "X";
                             Console.Clear();
                             ImagemJogoGalo(n1, n2, n3, n4, n5, n6, n7, n8, n9);
+                            if (n1 == n2 && n2 == n3||n1==n4 && n4==n7||n1==n5 && n1==n9)
+                            {
+                                Console.WriteLine("O X ganhou!");
+                                return;
+                            }
                             break;
                         case "2":
                             n2 = "X";
                             Console.Clear();
                             ImagemJogoGalo(n1, n2, n3, n4, n5, n6, n7, n8, n9);
+                            if (n2 == n1 && n2 == n3 || n2 == n5 && n5 == n8)
+                            {
+                                Console.WriteLine("O X ganhou!");
+                                return;
+                            }
                             break;
                         case "3":
                             n3 = "X";
                             Console.Clear();
                             ImagemJogoGalo(n1, n2, n3, n4, n5, n6, n7, n8, n9);
+                            if (n3 == n2 && n2 == n1 || n3 == n6 && n6 == n9 || n3 == n5 && n5 == n7)
+                            {
+                                Console.WriteLine("O X ganhou!");
+                                return;
+                            }
                             break;
                         case "4":
                             n4 = "X";
                             Console.Clear();
                             ImagemJogoGalo(n1, n2, n3, n4, n5, n6, n7, n8, n9);
+                            if (n4 == n1 && n4 == n7 || n4 == n5 && n5 == n6)
+                            {
+                                Console.WriteLine("O X ganhou!");
+                                return;
+                            }
                             break;
                         case "5":
                             n5 = "X";
                             Console.Clear();
                             ImagemJogoGalo(n1, n2, n3, n4, n5, n6, n7, n8, n9);
+                            if (n1 == n5 && n5 == n9 || n2 == n5 && n5 == n8 || n3 == n5 && n5 == n7||n4==n5&&n5==n6)
+                            {
+                                Console.WriteLine("O X ganhou!");
+                                return;
+                            }
                             break;
                         case "6":
                             n6 = "X";
                             Console.Clear();
                             ImagemJogoGalo(n1, n2, n3, n4, n5, n6, n7, n8, n9);
+                            if (n3 == n6 && n6 == n9 || n6 == n5 && n5 == n4 )
+                            {
+                                Console.WriteLine("O X ganhou!");
+                                return;
+                            }
                             break;
                         case "7":
                             n7 = "X";
                             Console.Clear();
                             ImagemJogoGalo(n1, n2, n3, n4, n5, n6, n7, n8, n9);
+                            if (n7 == n4 && n4 == n1 || n7 == n8 && n8 == n9)
+                            {
+                                Console.WriteLine("O X ganhou!");
+                                return;
+                            }
                             break;
                         case "8":
                             n8 = "X";
                             Console.Clear();
                             ImagemJogoGalo(n1, n2, n3, n4, n5, n6, n7, n8, n9);
+                            if (n8 == n5 && n5 == n2 || n7 == n8 && n8 == n9)
+                            {
+                                Console.WriteLine("O X ganhou!");
+                                return;
+                            }
                             break;
                         case "9":
                             n9 = "X";
                             Console.Clear();
                             ImagemJogoGalo(n1, n2, n3, n4, n5, n6, n7, n8, n9);
+                            if (n9 == n6 && n6 == n3 || n9 == n8 && n8 == n7 )
+                            {
+                                Console.WriteLine("O X ganhou!");
+                                return;
+                            }
                             break;
 
                         default:
@@ -123,46 +169,91 @@ namespace JogoGalo
                             n1 = "O";
                             Console.Clear();
                             ImagemJogoGalo(n1, n2, n3, n4, n5, n6, n7, n8, n9);
+                            if (n1 == n2 && n2 == n3 || n1 == n4 && n4 == n7 || n1 == n5 && n1 == n9)
+                            {
+                                Console.WriteLine("O O ganhou!");
+                                return;
+                            }
                             break;
                         case "2":
                             n2 = "O";
                             Console.Clear();
                             ImagemJogoGalo(n1, n2, n3, n4, n5, n6, n7, n8, n9);
+                            if (n1 == n2 && n2 == n3 || n2 == n5 && n5 == n8)
+                            {
+                                Console.WriteLine("O O ganhou!");
+                                return;
+                            }
                             break;
                         case "3":
                             n3 = "O";
                             Console.Clear();
                             ImagemJogoGalo(n1, n2, n3, n4, n5, n6, n7, n8, n9);
+                            if (n1 == n2 && n2 == n3 || n3 == n6 && n6 == n9 || n3 == n5 && n5 == n7)
+                            {
+                                Console.WriteLine("O O ganhou!");
+                                return;
+                            }
                             break;
                         case "4":
                             n4 = "O";
                             Console.Clear();
                             ImagemJogoGalo(n1, n2, n3, n4, n5, n6, n7, n8, n9);
+                            if (n1 == n4 && n4 == n7 || n4 == n5 && n5 == n6)
+                            {
+                                Console.WriteLine("O O ganhou!");
+                                return;
+                            }
                             break;
                         case "5":
                             n5 = "O";
                             Console.Clear();
                             ImagemJogoGalo(n1, n2, n3, n4, n5, n6, n7, n8, n9);
+                            if (n1 == n5 && n5 == n9 || n2 == n5 && n5 == n8 || n3 == n5 && n5 == n7 || n4 == n5 && n5 == n6)
+                            {
+                                Console.WriteLine("O O ganhou!");
+                                return;
+                            }
                             break;
                         case "6":
                             n6 = "O";
                             Console.Clear();
                             ImagemJogoGalo(n1, n2, n3, n4, n5, n6, n7, n8, n9);
+                            if (n3 == n6 && n6 == n9 || n6 == n5 && n5 == n4)
+                            {
+                                Console.WriteLine("O O ganhou!");
+                                return;
+                            }
                             break;
                         case "7":
                             n7 = "O";
                             Console.Clear();
                             ImagemJogoGalo(n1, n2, n3, n4, n5, n6, n7, n8, n9);
+                            if (n7 == n4 && n4 == n1 || n7 == n8 && n8 == n9)
+                            {
+                                Console.WriteLine("O O ganhou!");
+                                return;
+                            }
                             break;
                         case "8":
                             n8 = "O";
                             Console.Clear();
                             ImagemJogoGalo(n1, n2, n3, n4, n5, n6, n7, n8, n9);
+                            if (n8 == n5 && n5 == n2 || n7 == n8 && n8 == n9)
+                            {
+                                Console.WriteLine("O O ganhou!");
+                                return;
+                            }
                             break;
                         case "9":
                             n9 = "O";
                             Console.Clear();
                             ImagemJogoGalo(n1, n2, n3, n4, n5, n6, n7, n8, n9);
+                            if (n9 == n6 && n6 == n3 || n9 == n8 && n8 == n7)
+                            {
+                                Console.WriteLine("O O ganhou!");
+                                return;
+                            }
                             break;
                         default:
                             Console.Clear();
